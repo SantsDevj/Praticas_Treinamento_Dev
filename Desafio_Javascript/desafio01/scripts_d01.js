cd2376e996888fc751fb35306248335c883e2ee7
Santos Eduardo Júnior <santoseduardojuniors01@gmail.com>
2025-05-31 21:11:58 +0200

    Esta foi a última mudança que realizei

---
+++ Desafio_Javascript/desafio01/scripts_d01.js
@@ -0,0 +1,19 @@
+let d1 = document.querySelector('.d01')
+let d2 = document.querySelector('.d02')
+let d3 =  document.querySelector('.d03')
+
+function MostrarMsg1(){
+    if(d1){
+        window.alert('Clicou no Botão 1')
+    }
+}
+function MostrarMsg2(){
+    if(d2){
+        window.alert('Clicou no Botão 2')
+    }
+}
+function MostrarMsg3(){
+    if(d3){
+        window.alert('Clicou no Botão 3')
+    }
+}
