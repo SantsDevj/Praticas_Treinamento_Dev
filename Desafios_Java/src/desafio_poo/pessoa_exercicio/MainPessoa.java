5931ab52f7886cffce904a84e3988704c7a2b7d3
Santos Eduardo Júnior <santoseduardojuniors01@gmail.com>
2025-06-26 16:54:47 +0200

    resoução do último exercício de javaScript

---
+++ Desafios_Java/src/desafio_poo/pessoa_exercicio/MainPessoa.java
@@ -0,0 +1,40 @@
+package desafio_poo.pessoa_exercicio;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class MainPessoa {
+    public static void main(String[] args) {
+        //Chamando a classe pessoa
+        Pessoa[] pers = new Pessoa[10];
+        //Importando recursos 
+        Scanner readvalue = new Scanner(System.in);
+
+        //Início do programa
+        System.out.println("--------------------");
+        System.out.println("|-----Bem-Vindo-----|");
+        System.out.println("--------------------");
+        System.out.println("Insira os seus Dados.");
+        //Inicializando as variáveis
+        String nome =  "";
+        int idade = 0;
+        System.out.println("Nome:");
+        nome = readvalue.next();
+        try{
+            System.out.println("Idade:");
+            idade = readvalue.nextInt();
+        } catch(InputMismatchException excep){
+            System.out.println("Valor Inválido");
+        }
+        System.out.println("Em que posição pretendes guardar estes dados? \n De 1 a 10");
+        int pos = 0;
+        try {
+            pos = readvalue.nextInt();
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Índice Inválido");
+        }
+        pers[pos] = new Pessoa(nome, idade);
+        readvalue.close();
+        pers[pos].mostrarDetails();
+    }
+}
---
+++ Desafios_Java/src/desafio_poo/pessoa_exercicio/Pessoa.java
@@ -0,0 +1,36 @@
+package desafio_poo.pessoa_exercicio;
+
+public class Pessoa {
+    private String nome;
+    private  int idade;
+
+    /*Métodos Especiais */
+    //Constructor
+    public Pessoa(String nome, int idade){
+        setIdade(idade);
+        setNome(nome);
+    }
+    //Getters e Setters
+    //Nome
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    //Idade
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    //Métodos públicos
+    public void mostrarDetails(){
+        System.out.println("--------------------");
+        System.out.println("O seu nome: " + getNome());
+        System.out.println("A sua idade é: " + getIdade());
+        System.out.println("--------------------");
+    }
+}
